27eb6ddc14e5ccb4c99a0eac5f09af7760b4282f
cheryllin <10556021@ntub.edu.tw>
2020-05-28 01:55:27 +0800

    20200528-edit

--- app/src/main/java/com/cheryl/petit/help.java
+++ app/src/main/java/com/cheryl/petit/help.java
@@ -7,15 +7,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class help extends Fragment {
+public class  help extends Fragment {
     ViewPager viewPager;
     Adapter adapter;
     List<Model> models;
@@ -27,6 +30,8 @@ public class help extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_help, container, false);
+
+
 
 
         models = new ArrayList<>();
--- app/src/main/java/com/cheryl/petit/supp.java
+++ app/src/main/java/com/cheryl/petit/supp.java
@@ -1,21 +1,52 @@
 package com.cheryl.petit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class supp extends AppCompatActivity {
     private ImageButton back;
-
+    private Button choosedate;
+    private EditText date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_supp);
-
+        choosedate = findViewById(R.id.choosedate);
         back = findViewById(R.id.back);
+        date = findViewById(R.id.date);
+        choosedate = findViewById(R.id.choosedate);
+
+        choosedate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                new DatePickerDialog(supp.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        String dateTime = String.valueOf(year)+"-"+String.valueOf(month)+"-"+String.valueOf(day);
+                        date.setText(dateTime);
+
+                    }
+                },year,month,day).show();
+
+            }
+        });
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,4 +58,12 @@ public class supp extends AppCompatActivity {
             }
         });
     }
+
+
+    public void datePicker(View v){
+        Calendar calendar = Calendar.getInstance();
+
+
+    }
+
 }
