31d577c2cb637924eac0a6c90f0fa00f026022d7
cheryllin <10556021@ntub.edu.tw>
2020-05-29 20:19:58 +0800

    20200529-2

--- app/src/main/java/com/cheryl/petit/doc.java
+++ app/src/main/java/com/cheryl/petit/doc.java
@@ -1,21 +1,74 @@
 package com.cheryl.petit;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import java.util.Calendar;
 
 public class doc extends AppCompatActivity {
     private ImageButton back;
+    private Button docday,backdocbtn;
+    private EditText date;
+    private RadioButton yes,no;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doc);
-
+        docday = findViewById(R.id.docday);
         back = findViewById(R.id.back);
+        date = findViewById(R.id.date);
+        docday = findViewById(R.id.docday);
+        backdocbtn = findViewById(R.id.backdocbtn);
+        yes = findViewById(R.id.yes);
+        no = findViewById(R.id.no);
+
+        docday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                new DatePickerDialog(doc.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        String dateTime = String.valueOf(year)+"-"+String.valueOf(month)+"-"+String.valueOf(day);
+                        date.setText(dateTime);
+
+                    }
+                },year,month,day).show();
+
+            }
+        });
+
+
+        backdocbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar d = Calendar.getInstance();
+                int year = d.get(Calendar.YEAR);
+                int month = d.get(Calendar.MONTH);
+                int day = d.get(Calendar.DAY_OF_MONTH);
+                new DatePickerDialog(doc.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        String dateTime = String.valueOf(year)+"-"+String.valueOf(month)+"-"+String.valueOf(day);
+                        date.setText(dateTime);
+
+                    }
+                },year,month,day).show();
+
+            }
+        });
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +79,7 @@ public class doc extends AppCompatActivity {
                 finish();
             }
         });
+
     }
+
 }
