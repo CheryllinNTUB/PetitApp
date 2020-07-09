53e7b3d623d953db13cc7096d66d4ddda2531e1e
cheryllin <10556021@ntub.edu.tw>
2020-07-10 00:29:13 +0800

    20200710

--- app/src/main/java/com/cheryl/petit/petadd.java
+++ app/src/main/java/com/cheryl/petit/petadd.java
@@ -1,20 +1,51 @@
 package com.cheryl.petit;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.Calendar;
+import java.util.IllegalFormatCodePointException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class petadd extends AppCompatActivity {
     private ImageButton back;
+    private CircleImageView head;
+    private static final int GALLER_ACTION_PICK_CODE = 100;
+    Uri imageuri;
+    private EditText birthday;
+    private DatePickerDialog.OnDateSetListener dateSetListener;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_petadd);
 
         back =findViewById(R.id.back);
+        birthday = findViewById(R.id.birthday);
+        head = findViewById(R.id.head);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +55,91 @@ public class petadd extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        birthday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(
+                        petadd.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        dateSetListener, year, month, day
+                );
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+
+            }
+        });
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+
+                month = month + 1;
+
+                String thedate = year + "/" + month + "/" + day;
+
+                birthday.setText(thedate);
+            }
+        };
+        //更換大頭照
+        head.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                runTimePermisson();
+            }
+        });
     }
+
+    private void runTimePermisson(){
+
+        Dexter.withContext(this).
+                withPermission(
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                ).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                galleryIntent();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken token) {
+                token.continuePermissionRequest();
+            }
+        }).check();
+
+    }
+
+    private void galleryIntent() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,GALLER_ACTION_PICK_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if (resultCode == RESULT_OK){
+
+            if(requestCode == GALLER_ACTION_PICK_CODE){
+
+                imageuri = data.getData();
+                head.setImageURI(imageuri);
+
+            }
+        }
+    }
+
 }
